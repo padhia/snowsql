72e95c624d5cbc3a3ed47b359a2e8cfb07a7d267
Paresh Adhia <padhia@gmail.com>
2024-01-30 13:35:46 -0500

    PSA: this flake is being deprecated

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # snowsql Nix Flake
 
+**This flake is superseded by [Snwoflake nix flake](https://github.com/padhia/snowflake).** Addtionally, the new flake provides an overlay for `snowsql`.
+
 This flake now contains, usually, a more up-to-date version of `snowsql`.
 
 Historically, the flake provided a patched version of `snowsql` that fixed [this issue](https://github.com/NixOS/nixpkgs/issues/199622). Starting with snowsql version [1.2.30](https://docs.snowflake.com/en/release-notes/clients-drivers/snowsql-2023#version-1-2-30-november-13-2023),  the underlying Snowflake Python connector no longer depends on `oscrypto`, which was needed to be patched.
--- flake.nix
+++ flake.nix
@@ -1,22 +1,13 @@
 {
-  description = "snowsql with binutils in the path";
+  description = "Snowflake snowsql command-line SQL utility";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    snowflake.url = "github:padhia/snowflake";
+    snowflake.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs}:
-  let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      config = { allowUnfree = true; };
-    };
-
-    snowsql = pkgs.callPackage ./snowsql.nix {};
-
-  in {
-    apps.x86_64-linux.default = {
-      type = "app";
-      program = "${snowsql}/bin/snowsql";
-    };
-    packages.x86_64-linux.default = snowsql;
+  outputs = { self, nixpkgs, snowflake }: {
+    apps.x86_64-linux.default = snowflake.apps.x86_64-linux.snowsql;
   };
 }
